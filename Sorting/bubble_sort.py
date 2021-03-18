603ce2335029e38df7e11bdba65593845150bb85
johannes-pirmann <johannes.pirmann@gmail.com>
2021-03-18 08:46:28 +0100

    Optimized Bubble Sort
    Signed-off-by: johannes-pirmann <johannes.pirmann@gmail.com>

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -5,14 +5,14 @@ def bubble_sort(list):
     """
     Worst case complexity of O(n)²
     """
-    for i in range(0, len(list)-1):
+    n = len(list)
+    for i in range(0, n-1):
         swapped = False
-        for j in range(0, len(list)-1):
+        for j in range(0, n-i-1):  
+            # '-i' AS the last element is already sorted.
             next_item = j+1
             if list[j] > list[next_item]:
-                tmp = list[j]
-                list[j] = list[next_item]
-                list[j+1] = tmp
+                list[j], list[j+1] = list[j+1], list[j]
                 swapped = True
 
         if swapped == False:
